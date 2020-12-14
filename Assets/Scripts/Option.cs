65ae22237e796be5a182e46b908882076895be57
Kylin14570 <2813378364@qq.com>
2020-12-14 20:42:28 +0800

    OptionFinished

--- Assets/Scripts/Option.cs
+++ Assets/Scripts/Option.cs
@@ -1,37 +1,53 @@
-﻿using System.Collections;
-using System.Collections.Generic;
+﻿/********************
+ * 选择游戏模式
+ * ****************/
+
 using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
 using UnityEngine.SceneManagement;
 
-public class Option : MonoBehaviour {
+public class Option : MonoBehaviour
+{
 
-    private int choice = 1;
     public Transform posOne;
     public Transform posTwo;
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
+    private int op = 1;
+    // 游戏模式选择变量
+    // op=1 表示选择单人模式
+    // op=2 表示选择双人模式
+
+    // Use this for initialization
+    void Start()
+    {
+        //nothing to be done
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // 输入W键表示选择单人模式
         if (Input.GetKeyDown(KeyCode.W))
         {
-            choice = 1;
+            op = 1;
             transform.position = posOne.position;
         }
+        // 输入S键表示选择双人模式
         else if (Input.GetKeyDown(KeyCode.S))
         {
-            choice = 2;
+            op = 2;
             transform.position = posTwo.position;
         }
-        if (choice==1&&Input.GetKeyDown(KeyCode.Space))
+        // 空格键确认选择，进入游戏
+        if (op == 1 && Input.GetKeyDown(KeyCode.Space))
         {
+            //单人模式进入场景1
             SceneManager.LoadScene(1);
         }
-        if (choice == 2 && Input.GetKeyDown(KeyCode.Space))
+        if (op == 2 && Input.GetKeyDown(KeyCode.Space))
         {
+            //双人模式进入场景2
             SceneManager.LoadScene(2);
         }
     }
